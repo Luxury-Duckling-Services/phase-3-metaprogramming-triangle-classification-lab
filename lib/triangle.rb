01f6b9eee03a33042a6b61145f6bda7080c421fa
Yifan <datasciencewyfan@gmail.com>
2022-06-28 11:12:25 -0700

    commit tree 8159ac32a0203b159b5c2091c99abcf6dda0291d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
   # write code here
+
+  attr_accessor :a , :b , :c
+
+  def initialize(a,b,c)
+    @a=[a , b , c].sort() [0]
+    @b=[a , b , c].sort() [1]
+    @c=[a , b , c].sort() [2]
+  end
+
+  def kind
+    if self.is_triangle?
+      if @a==@c
+        return :equilateral
+      elsif @a==@b or @b==@c
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  def is_triangle?
+    if a <=0 or a+b<=c
+      return false
+    else
+      return true
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid triangle."
+    end
+  end
+
 end
